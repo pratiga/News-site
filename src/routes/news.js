a388eca7c98e76ea475027e41b27e3cb166489df
Pratima <gaitrisanu2468@gmail.com>
2022-06-16 22:22:05 +0545

    add news route
    Signed-off-by: Pratima <gaitrisanu2468@gmail.com>

--- src/routes/news.js
+++ src/routes/news.js
@@ -11,8 +11,10 @@ const newsRouter = express.Router()
       'country=in&' +
       'apiKey=36f3e29b704f41339af8439dc1228334';
       const news_get =await axios.get(url)
+      console.log(news_get.data)
+      
       res.render('partials/news',{articles:news_get.data.articles})
-     } catch (err){
+           } catch (err){
          
         if(err.response){
            //res.render('news', {articles : null })
@@ -30,30 +32,30 @@ const newsRouter = express.Router()
      
      
  })
-// //     try {
-//          const newsAPI = await axios.get('http://newsapi.org/v2/top-headlines?' +
-//          'country=in&' +
-//          'apiKey=c6aa863b9af1464990d9f9651c17cfdd')
-       
-
-// //         res.render('news', {articles : newsAPI.data })
-//               console.log(newsAPI.data)
-//                 } catch (error) 
-//         if(err.response){
-//             res.render('news', {articles : null })
-//             console.log(err.response.data)
-//             console.log(err.response.status)
-//             console.log(err.response.headers) 
-//         } else if(err.requiest){
-//             res.render('news', { articles : null })
-//             console.log(err.requiest)
-//         }else {
-//             res.render('news', {articles : null })
-//             console.error('Error', err.message)
-//         }
-//     }
+ 
+ newsRouter.get('/:id', async(req,res)=> {
+    let articleID = req.params.id
     
-    
-// })
+    try {
+        var url = 'http://newsapi.org/v2/top-headlines?' +
+      'country=in&' +
+      'apiKey=36f3e29b704f41339af8439dc1228334';
+        const newsAPI = await axios.get(url)
+        res.render('newsSingle', { article : newsAPI.data})
+    } catch (err) {
+        if(err.response){
+            res.render('newsSingle', { article : null })
+            console.log(err.response.data)
+            console.log(err.response.status)
+            console.log(err.response.headers)
+        } else if(err.requiest){
+            res.render('newsSingle', {article : null})
+            console.log(err.requiest)
+        } else {
+            res.render('newsSingle',{ article : null })
+            console.error('Error', err.message)
+        }
+    }
+ })
 
 module.exports = newsRouter
